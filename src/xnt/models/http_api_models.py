b3f8c1d968ec8ee3463b290e9b9ab96837866fda
Tomasz Brzezina <tomasz@brzezina.pl>
2025-09-04 19:39:37 +0200

    Transaction V1 fix

--- src/xnt/models/http_api_models.py
+++ src/xnt/models/http_api_models.py
@@ -378,12 +378,12 @@ class SummaryV3(Serializable):
 
 SummaryType = TypeVar('SummaryType', SummaryV1, SummaryV2, SummaryV3, covariant=True)
 
-
 class TransactionV1(Serializable):
-    def __init__(self, operation_type: str, id_: str, asset: Optional[str], when: int, sum_: float,
-                 symbol_id: Optional[str] = None, account_id: Optional[str] = None,
-                 order_id: Optional[str] = None, order_pos: Optional[int] = None,
-                 uuid_: Optional[str] = None, value_date: Optional[str] = None) -> None:
+    def init(self, operation_type: str, id_: str, asset: Optional[str], when: int, sum_: float,
+             symbol_id: Optional[str] = None, account_id: Optional[str] = None, value_date: Optional[str] = None,
+             order_id: Optional[str] = None, order_pos: Optional[int] = None,
+             uuid_: Optional[str] = None, parent_uuid: Optional[str] = None,
+             transaction_price: Optional[int] = None) -> None:
         self.operation_type = operation_type
         self.id_ = id_
         self.asset = asset
@@ -394,7 +394,8 @@ class TransactionV1(Serializable):
         self.order_id = order_id
         self.order_pos = dc(order_pos)
         self.uuid = uuid_
-        self.value_date = date.fromisoformat(value_date) if value_date else None  # type: date
+        self.parent_uuid = parent_uuid
+        self.transaction_price = transaction_price
 
 
 class TransactionV2(TransactionV1):
